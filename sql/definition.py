4e0b36749f0289c2ab67388aeba7b929026db65d
Eduardo Guadalupe Méndez Miranda <lalorayado1@live.com.mx>
2023-06-09 16:57:17 -0600

    Moved database to sql folder and fixed foreign keys problem

--- sql/definition.py
+++ sql/definition.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine, Column, String, Integer, Date, Float, ForeignKey
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import event
 
 Base = declarative_base()
 
-engine = create_engine('sqlite:///school.db', echo=True)
+engine = create_engine('sqlite:///sql/school.db', echo=True)
 
 class Student(Base):
     __tablename__ = 'Students'
@@ -15,14 +16,15 @@ class Student(Base):
     birthday = Column(Date,nullable=False)
     semester = Column(Integer,nullable=False)
     gpa = Column(Float,nullable=False)
-    carrerId = Column(Integer,ForeignKey('Carrers.id'),nullable=False)
+    carrerId = Column(Integer,ForeignKey('Carrers.id', ondelete='CASCADE', onupdate='CASCADE'),nullable=False)
+    
 
 class Asignature(Base):
     __tablename__ = 'Asignatures'
     id = Column(Integer, primary_key=True)
     name = Column(String(80),nullable=False,unique=True)
     semester = Column(Integer,nullable=False)
-    carrerId = Column(Integer,ForeignKey('Carrers.id'), nullable = False)
+    carrerId = Column(Integer,ForeignKey('Carrers.id', ondelete='CASCADE', onupdate='CASCADE'), nullable = False)
 
 
 class Teacher(Base):
@@ -42,17 +44,18 @@ class Carrer(Base):
 class StudentAsignature(Base):
     __tablename__ = 'StudentAsignatures'
     id = Column(Integer, primary_key=True)
-    idStudent = Column(Integer,ForeignKey('Students.id'),nullable = False)
-    idAsignature = Column(Integer,ForeignKey('Asignatures.id') ,nullable = False)
+    idStudent = Column(Integer,ForeignKey('Students.id', ondelete='CASCADE', onupdate='CASCADE'),nullable = False)
+    idAsignature = Column(Integer,ForeignKey('Asignatures.id', ondelete='CASCADE', onupdate='CASCADE') ,nullable = False)
 
 class TeacherAsignature(Base):
     __tablename__ = 'TeacherAsignatures'
     id = Column(Integer, primary_key=True)
     hour = Column(String(30),nullable = False)
     groupNo = Column(Integer, nullable = False, unique = True)
-    idTeacher = Column(Integer,ForeignKey('Teachers.id'),nullable = False)
-    idAsignature = Column(Integer,ForeignKey('Asignatures.id'),nullable = False)
+    idTeacher = Column(Integer,ForeignKey('Teachers.id', ondelete='CASCADE', onupdate='CASCADE'),nullable = False)
+    idAsignature = Column(Integer,ForeignKey('Asignatures.id', ondelete='CASCADE', onupdate='CASCADE'),nullable = False)
 
 
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
+event.listen(engine, 'connect', lambda c, _: c.execute('pragma foreign_keys=on'))
