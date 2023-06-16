1c3b72a25b26ae49ee42f3a9dd9d6a6dccaa3bdb
Eduardo Guadalupe Méndez Miranda <lalorayado1@live.com.mx>
2023-06-16 14:45:30 -0600

    Base models updated in definition

--- sql/definition.py
+++ sql/definition.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Integer, Date, Float, ForeignKey
+from sqlalchemy import create_engine, Column, String, Integer, Date, Float, ForeignKey, CheckConstraint
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import event
@@ -17,7 +17,15 @@ class Student(Base):
     semester = Column(Integer,nullable=False)
     gpa = Column(Float,nullable=False)
     careerId = Column(Integer,ForeignKey('Careers.id', ondelete='CASCADE', onupdate='CASCADE'),nullable=False)
-    
+    __table_args__ = (
+        CheckConstraint(studentId >= 10000, name='studentId_format'),
+    )
+    __table_args__ = (
+        CheckConstraint(semester.between(1,10), name='number_semesters'),
+    )
+    __table_args__ = (
+        CheckConstraint(semester.between(1,100), name='gpa_range'),
+    )
 
 class Asignature(Base):
     __tablename__ = 'Asignatures'
@@ -25,7 +33,9 @@ class Asignature(Base):
     name = Column(String(80),nullable=False,unique=True)
     semester = Column(Integer,nullable=False)
     careerId = Column(Integer,ForeignKey('Careers.id', ondelete='CASCADE', onupdate='CASCADE'), nullable = False)
-
+    __table_args__ = (
+        CheckConstraint(semester.between(1,10), name='number_semesters'),
+    )
 
 class Teacher(Base):
     __tablename__ = 'Teachers'
@@ -33,6 +43,9 @@ class Teacher(Base):
     employeeId = Column(Integer, nullable = False, unique = True)
     firstName = Column(String(50), nullable = False)
     secondName = Column(String(50), nullable = False)
+    __table_args__ = (
+        CheckConstraint(employeeId >= 1000, name='employeeId_format'),
+    )
 
 class Career(Base):
     __tablename__ = 'Careers'
@@ -41,21 +54,21 @@ class Career(Base):
     students = relationship('Student', backref='career')
     asignatures = relationship('Asignature', backref='career')
 
-class StudentAsignature(Base):
-    __tablename__ = 'StudentAsignatures'
+class StudentClass(Base):
+    __tablename__ = 'StudentClasses'
     id = Column(Integer, primary_key=True,autoincrement=True)
     idStudent = Column(Integer,ForeignKey('Students.id', ondelete='CASCADE', onupdate='CASCADE'),nullable = False)
-    idAsignature = Column(Integer,ForeignKey('Asignatures.id', ondelete='CASCADE', onupdate='CASCADE') ,nullable = False)
+    idClass = Column(Integer,ForeignKey('Classes.id', ondelete='CASCADE', onupdate='CASCADE') ,nullable = False)
 
-class TeacherAsignature(Base):
-    __tablename__ = 'TeacherAsignatures'
+class Class(Base):
+    __tablename__ = 'Classes'
     id = Column(Integer, primary_key=True,autoincrement=True)
     hour = Column(String(30),nullable = False)
     groupNo = Column(Integer, nullable = False, unique = True)
     idTeacher = Column(Integer,ForeignKey('Teachers.id', ondelete='CASCADE', onupdate='CASCADE'),nullable = False)
     idAsignature = Column(Integer,ForeignKey('Asignatures.id', ondelete='CASCADE', onupdate='CASCADE'),nullable = False)
+    
 
-
-#Base.metadata.drop_all(engine)
-#Base.metadata.create_all(engine)
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
 #event.listen(engine, 'connect', lambda c, _: c.execute('pragma foreign_keys=on'))
